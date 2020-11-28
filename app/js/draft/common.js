2cb2b12c030efb39808371ca7b91dff9fa784277
Aleksandr Tereshchuk <atereshhuk68@ukr.net>
2020-11-28 17:34:41 +0200

    TheLoop. Header. Responsive

---
+++ app/js/draft/common.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+	//! Mobile menu
+	const hamburger = document.querySelector(".hamburger");
+	const mobileMenu = document.querySelector(".mobile-menu");
+
+	const toggleMobileMenu = () => {
+		hamburger.classList.toggle("is-active");
+		mobileMenu.classList.toggle("mobile-menu--active");
+		document.body.classList.toggle("no-scroll");
+	};
+
+	hamburger.addEventListener("click", toggleMobileMenu);
+});
